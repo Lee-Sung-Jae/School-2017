78d47aeee5494e8630097007c07d4cccf49c9954
Lee Sung Jae <me@lsj.pink>
2017-05-08 14:12:44 +0900

    T0508_3

---
+++ T0508_3/T0508_3/T0508_3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("30 + 20 = %d\n", 30 + 20);
+	printf("30 * 20 =  %d\n", 30 * 20);
+	printf("10 / 3 = %f\n", 10 / 3);
+	printf("10 / 3.0 = %f\n", 10 / 3.0);
+	printf("-10 %% 3 = %d\n", -10 % 3);
+	printf("10 ^ 3 = %d\n", 10 * 10 * 10);
+	
+	return 0;
+}
