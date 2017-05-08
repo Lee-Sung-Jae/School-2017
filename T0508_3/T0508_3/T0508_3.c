5d1d590558db4630690a9a89312cce2e5d96b733
Lee Sung Jae <me@lsj.pink>
2017-05-08 14:19:51 +0900

    T0508_3

--- T0508_3/T0508_3/T0508_3.c
+++ T0508_3/T0508_3/T0508_3.c
@@ -3,7 +3,9 @@
 int main(void) {
 	printf("30 + 20 = %d\n", 30 + 20);
 	printf("30 * 20 =  %d\n", 30 * 20);
-	printf("10 / 3 = %f\n", 10 / 3);
+	//Orignal Code is `%f`, but teacher said change to `%d`.
+	//If do original, return `0.000000`, which in book example return value.
+	printf("10 / 3 = %d\n", 10 / 3);
 	printf("10 / 3.0 = %f\n", 10 / 3.0);
 	printf("-10 %% 3 = %d\n", -10 % 3);
 	printf("10 ^ 3 = %d\n", 10 * 10 * 10);
