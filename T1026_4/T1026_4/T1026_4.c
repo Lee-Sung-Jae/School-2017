d49c59c79de7704be37a93b6f425aff7513fcd5f
Lee Sung Jae <me@lsj.pink>
2017-10-26 15:12:32 +0900

    T1026_4

---
+++ T1026_4/T1026_4/T1026_4.c
@@ -0,0 +1,35 @@
+// Page 196, Problem 3.
+
+#include <stdio.h>
+
+static int point;
+
+void input(int data[], int value) {
+	data[point++] = value;
+}
+
+int output(int * data) {
+	return * (data + --point);
+}
+
+int main (void) {
+	static int data[5];
+	int a, b, c;
+
+	input(data, 100);
+	input(data, 100);
+	input(data, 100);
+	input(data, 100);
+
+	a = output(data);
+	b = output(data);
+
+	input(data, 500);
+	input(data, 600);
+
+	c = output(data);
+
+	printf("%d\n", a + b + c);
+
+	return 0;
+}
